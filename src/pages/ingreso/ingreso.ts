7a143894d42830b8d11a9978b7bbeee032848ef9
rodri090 <l2odri090@gmail.com>
2018-10-14 22:36:09 -0400

    segundo commit

--- src/pages/ingreso/ingreso.ts
+++ src/pages/ingreso/ingreso.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { UsuariosPage } from'../usuarios/usuarios';
 @IonicPage()
 @Component({
   selector: 'page-ingreso',
@@ -14,5 +14,10 @@ export class IngresoPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad IngresoPage');
   }
+
+  usuario(){
+    this.navCtrl.push(UsuariosPage);
+}
+
 
 }
